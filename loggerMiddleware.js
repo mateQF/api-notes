f7eeaba2414ec4fe30a7e57fe508e0f7f1183ecd
Mateo <101014746+mateQF@users.noreply.github.com>
2023-01-08 11:02:53 -0300

    Update logger

--- loggerMiddleware.js
+++ loggerMiddleware.js
@@ -3,7 +3,7 @@ const logger = (req, res, next) => {
   console.log(req.path)
   console.log(req.body)
   console.log('------')
-  next() // para que vaya a la siguiene ruta y no se quede ahi esperando
+  next()
 }
 
 module.exports = logger
